7ac2caf96c32f77a702ec0aef6ed72486b1c83fb
LitecoinZ <team@litecoinz.info>
2019-04-11 19:00:48 +0200

    New version by Newtl

--- js/chng-lang.js
+++
@@ -1,7 +0,0 @@
-var userLang = navigator.language || navigator.userLanguage;
-if (userLang.substring(0,2) == 'es'){
-    window.location.href = '/es';
-}
-if (userLang.substring(0,2) == 'en'){
-    window.location.href = '/';
-}
